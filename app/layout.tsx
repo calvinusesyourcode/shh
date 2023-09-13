eeee37abfce11c3d4e7ef7dad498fcd38111af05
calvin <calvinducharme@gmail.com>
2023-09-13 10:34:15 -0700

    hmm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import "@/styles/globals.css"
 import { Metadata } from "next"
 
@@ -7,29 +9,20 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { MyContext } from "@/lib/context";
+import { useEffect, useState } from "react";
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-}
 
 interface RootLayoutProps {
   children: React.ReactNode
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const [peerConnection, setPeerConnection] = useState<RTCPeerConnection | null>(null);
+  const [localStream, setLocalStream] = useState<any>(null);
+  const [remoteStream, setRemoteStream] = useState<any>(null);
+  
+  
   return (
     <>
       <html lang="en" suppressHydrationWarning>
@@ -41,11 +34,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
           )}
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <MyContext.Provider value={{peerConnection, setPeerConnection, localStream, setLocalStream, remoteStream, setRemoteStream}}>
             <div className="relative flex min-h-screen flex-col">
               <SiteHeader />
               <div className="flex-1">{children}</div>
             </div>
             <TailwindIndicator />
+            </MyContext.Provider>
           </ThemeProvider>
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,134 @@
+'use client';
+
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
+import { useContext } from "react";
+import { MyContext, servers } from "@/lib/context";
+import { db } from "@/lib/firebase";
+import { collection, doc, setDoc, onSnapshot, query, where, getDoc, updateDoc, addDoc } from "firebase/firestore";
+
 
 export default function IndexPage() {
+  let pc = new RTCPeerConnection(servers);
+  
+  let localStream: any = null;
+  let remoteStream: any = null;
+  const startWebcam = async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true});
+    remoteStream = new MediaStream();
+    
+    localStream.getTracks().forEach((track: any) => {
+      pc.addTrack(track, localStream);
+    });
+
+    pc.ontrack = (e: any) => {
+      e.streams[0].getTracks().forEach((track: any) => {
+        remoteStream.addTrack(track, remoteStream);
+      });
+    }
+    const myWebcam: HTMLVideoElement = document.getElementById("my-webcam") as HTMLVideoElement;
+    const theirWebcam: HTMLVideoElement = document.getElementById("their-webcam") as HTMLVideoElement;
+
+    console.log(localStream);
+    myWebcam.srcObject = localStream;
+    myWebcam.play().catch(error => {
+      console.error(error)
+    });
+  }
+  const startCall = async () => {
+    const callDoc = collection(db, 'calls');
+    const offerCandidates = collection(doc(callDoc), 'offerCandidates');
+    const answerCandidates = collection(doc(callDoc), 'answerCandidates');
+    
+    
+    pc.onicecandidate = async (event) => {
+      if (event.candidate) {
+        let data = event.candidate.toJSON()
+        await setDoc(doc(offerCandidates), {data})
+      }
+    }
+
+    const offerDescription = await pc.createOffer();
+    await pc.setLocalDescription(offerDescription);
+
+    const offer = {
+      sdp: offerDescription.sdp,
+      type: offerDescription.type,
+    }
+
+    const newCallDoc = await addDoc(callDoc, { offer })
+    const callInputField: HTMLInputElement = document.getElementById("callInputField") as HTMLInputElement;
+    callInputField.value = newCallDoc.id;
+    
+    onSnapshot(doc(callDoc), (snapshot) => {
+      const data = snapshot.data();
+      if (!pc.currentRemoteDescription && data?.answer) {
+        const answerDescription = new RTCSessionDescription(data.answer);
+        pc.setRemoteDescription(answerDescription);
+      }
+    })
+
+    onSnapshot(query(answerCandidates, where("x", "==", "y")), (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === 'added') {
+          const candidate = new RTCIceCandidate(change.doc.data());
+          pc.addIceCandidate(candidate);
+        }
+      })
+    })
+    
+  }
+  const answerCall = async () => {
+    const callInputField: HTMLInputElement = document.getElementById("callInputField") as HTMLInputElement;
+    const callId = callInputField.value;
+    const callDoc = doc(collection(db, 'calls'), callId);
+    const answerCandidates = collection(callDoc, 'answerCandidates');
+    const offerCandidates = collection(callDoc, 'offerCandidates');
+
+    pc.onicecandidate = async (event) => {
+      if (event.candidate) {
+        const data = event.candidate.toJSON();
+        await setDoc(doc(answerCandidates), {data})
+      }
+    }
+
+    const callData: any = (await getDoc(callDoc)).data();
+    const offerDescription = callData.offer;
+    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+    const answerDescription = await pc.createAnswer();
+    await pc.setLocalDescription(answerDescription);
+
+    const answer = {
+      type: answerDescription.type,
+      sdp: answerDescription.sdp,
+    };
+
+    await updateDoc(callDoc, {answer});
+
+    onSnapshot(query(offerCandidates, where("x", "==", "y")), (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        console.log(change);
+        if (change.type === 'added') {
+          let data = change.doc.data();
+          pc.addIceCandidate(new RTCIceCandidate(data));
+        }
+      })
+    })
+  }
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
+      <Button onClick={() => {startWebcam()}}>start webcam</Button>
+      <Button onClick={() => {startCall()}}>start call</Button>
+      <input id="callInputField" />
+      <Button onClick={() => {answerCall()}}>answer call</Button>
+      <video id="my-webcam">
+      </video>
+      <video id="their-webcam">
+      </video>
     </section>
   )
 }
