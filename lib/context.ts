a91deef821a6f7f0e6edba864202052da1141d83
calvin <calvinducharme@gmail.com>
2023-09-14 15:48:53 -0700

    unstable: ?TURN SERVERS?

--- lib/context.ts
+++ lib/context.ts
@@ -12,10 +12,30 @@ interface MyContextType {
 export const MyContext = React.createContext<MyContextType | null>(null);
 
 export const servers = {
-    iceServers: [
+  iceServers: [
       {
-        urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+        urls: "stun:stun.relay.metered.ca:80",
       },
-    ],
+      {
+        urls: "turn:a.relay.metered.ca:80",
+        username: "546a8a72a60f269db3114243",
+        credential: "L0QjIY5fd7e6AG/i",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:80?transport=tcp",
+        username: "546a8a72a60f269db3114243",
+        credential: "L0QjIY5fd7e6AG/i",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:443",
+        username: "546a8a72a60f269db3114243",
+        credential: "L0QjIY5fd7e6AG/i",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:443?transport=tcp",
+        username: "546a8a72a60f269db3114243",
+        credential: "L0QjIY5fd7e6AG/i",
+      },
+  ],
     iceCandidatePoolSize: 10,
   };  
