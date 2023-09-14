6619ad7560bbd3d43df9a309b75e1237aff1a00c
calvin <calvinducharme@gmail.com>
2023-09-14 16:09:01 -0700

    unstable: turn server testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
 'use client';
 
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { Button } from "@/components/ui/button"
-import { useContext } from "react";
-import { MyContext, servers } from "@/lib/context";
-import { db } from "@/lib/firebase";
-import { collection, doc, setDoc, onSnapshot, query, where, getDoc, updateDoc, addDoc, getDocs, deleteDoc } from "firebase/firestore";
 import { Webcall } from "@/components/webcall";
 
 export default function IndexPage() {
