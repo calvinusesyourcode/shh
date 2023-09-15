48365fe9ce7d0fd565e0841abd2893b110136f45
calvin <calvinducharme@gmail.com>
2023-09-15 13:09:44 -0700

    unstable: still

--- components/audiocall.tsx
+++ components/audiocall.tsx
@@ -14,9 +14,9 @@ export function WebcallAsAdmin() {
 
   const [callIds, setCallIds] = useState<string[]>([]);
 
-
   const initMedia = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true});
+    console.log("init media local stream", localStream)
     const ref = doc(collection(db, 'calls'), 'newCalls')
   onSnapshot(ref, (snapshot) => {
     const data = snapshot.data()
@@ -29,163 +29,163 @@ export function WebcallAsAdmin() {
     });
   }
 
-  const connectAsGuest = async () => {
-    // await startWebcam()
-    const response = await fetch("https://piano.metered.live/api/v1/turn/credentials?apiKey="+process.env.NEXT_PUBLIC_TURN_SERVER_API_KEY);
-    const stunAndTurnServers = await response.json();
-    const servers: object = {
-      iceServers: stunAndTurnServers,
-        iceCandidatePoolSize: 10,
-      };
-    pc = new RTCPeerConnection(servers);
+  // const connectAsGuest = async () => {
+  //   // await startWebcam()
+  //   const response = await fetch("https://piano.metered.live/api/v1/turn/credentials?apiKey="+process.env.NEXT_PUBLIC_TURN_SERVER_API_KEY);
+  //   const stunAndTurnServers = await response.json();
+  //   const servers: object = {
+  //     iceServers: stunAndTurnServers,
+  //       iceCandidatePoolSize: 10,
+  //     };
+  //   pc = new RTCPeerConnection(servers);
     
-    localStream.getTracks().forEach((track: any) => {
-      pc.addTrack(track, localStream);
-    });
+  //   localStream.getTracks().forEach((track: any) => {
+  //     pc.addTrack(track, localStream);
+  //   });
 
-    // await answerCall()
-    let callId;
-    (await getDocs(query(collection(db, 'calls'), orderBy("createdAt","desc"), limit(1)))).forEach((doc => {callId = doc.id}))
-    // const callInputField: HTMLInputElement = document.getElementById("callInputField") as HTMLInputElement;
-    // callId = "DVBaSUUxPefEmai9B9Fd"
-    // if (callId) {callInputField.value = callId}
-    if (!callId) {
-        console.error("callId not found")
-    }
-    const callDoc = doc(collection(db, 'calls'), callId)
-    // const callDocs = query(collection(db, 'calls'))
-    // for (let i = 0, l = callDocs.length; i++) {}
+  //   // await answerCall()
+  //   let callId;
+  //   (await getDocs(query(collection(db, 'calls'), orderBy("createdAt","desc"), limit(1)))).forEach((doc => {callId = doc.id}))
+  //   // const callInputField: HTMLInputElement = document.getElementById("callInputField") as HTMLInputElement;
+  //   // callId = "DVBaSUUxPefEmai9B9Fd"
+  //   // if (callId) {callInputField.value = callId}
+  //   if (!callId) {
+  //       console.error("callId not found")
+  //   }
+  //   const callDoc = doc(collection(db, 'calls'), callId)
+  //   // const callDocs = query(collection(db, 'calls'))
+  //   // for (let i = 0, l = callDocs.length; i++) {}
     
-    const answerCandidates = collection(callDoc, 'answerCandidates');
-    const offerCandidates = collection(callDoc, 'offerCandidates');
+  //   const answerCandidates = collection(callDoc, 'answerCandidates');
+  //   const offerCandidates = collection(callDoc, 'offerCandidates');
 
-    pc.onicecandidate = async (event: any) => {
-      if (event.candidate) {
-        console.log({event_candidate2: event.candidate})
-        await setDoc(doc(answerCandidates), {...event.candidate.toJSON()})
-      }
-    }
+  //   pc.onicecandidate = async (event: any) => {
+  //     if (event.candidate) {
+  //       console.log({event_candidate2: event.candidate})
+  //       await setDoc(doc(answerCandidates), {...event.candidate.toJSON()})
+  //     }
+  //   }
 
-    const callData: any = (await getDoc(callDoc)).data();
-    const offerDescription = callData.offer;
-    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+  //   const callData: any = (await getDoc(callDoc)).data();
+  //   const offerDescription = callData.offer;
+  //   await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
 
-    const answerDescription = await pc.createAnswer();
-    await pc.setLocalDescription(answerDescription);
+  //   const answerDescription = await pc.createAnswer();
+  //   await pc.setLocalDescription(answerDescription);
 
-    const answer = {
-      type: answerDescription.type,
-      sdp: answerDescription.sdp,
-    };
+  //   const answer = {
+  //     type: answerDescription.type,
+  //     sdp: answerDescription.sdp,
+  //   };
 
-    await updateDoc(callDoc, {answer});
+  //   await updateDoc(callDoc, {answer});
 
-    onSnapshot(offerCandidates, (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        console.log(change);
-        if (change.type === 'added') {
-          console.log({change_doc_data_2: change.doc.data()})
-          let data = change.doc.data();
-          pc.addIceCandidate(new RTCIceCandidate(data));
-        }
-      })
-    })
-    // await showVideo()
-    console.log("PROVING TRACKS ARE AVAILABLE");
-    console.log(remoteStream.getTracks());
-    console.log(remoteStream);
-    const theirWebcam: HTMLVideoElement = document.getElementById("their-webcam") as HTMLVideoElement;
-    theirWebcam.srcObject = remoteStream;
+  //   onSnapshot(offerCandidates, (snapshot) => {
+  //     snapshot.docChanges().forEach((change) => {
+  //       console.log(change);
+  //       if (change.type === 'added') {
+  //         console.log({change_doc_data_2: change.doc.data()})
+  //         let data = change.doc.data();
+  //         pc.addIceCandidate(new RTCIceCandidate(data));
+  //       }
+  //     })
+  //   })
+  //   // await showVideo()
+  //   console.log("PROVING TRACKS ARE AVAILABLE");
+  //   console.log(remoteStream.getTracks());
+  //   console.log(remoteStream);
+  //   const theirWebcam: HTMLVideoElement = document.getElementById("their-webcam") as HTMLVideoElement;
+  //   theirWebcam.srcObject = remoteStream;
     
-    theirWebcam.play().catch(error => {
-      console.error(error)
-    });
-  }
-  const connectAsHost = async () => {
-    //startWebcam
-    const response = await fetch("https://piano.metered.live/api/v1/turn/credentials?apiKey="+process.env.NEXT_PUBLIC_TURN_SERVER_API_KEY);
-    const stunAndTurnServers = await response.json();
-    const servers: object = {
-      iceServers: stunAndTurnServers,
-        iceCandidatePoolSize: 10,
-      };  
-    pc = new RTCPeerConnection(servers);
-    remoteStream = new MediaStream();
+  //   theirWebcam.play().catch(error => {
+  //     console.error(error)
+  //   });
+  // }
+  // const connectAsHost = async () => {
+  //   //startWebcam
+  //   const response = await fetch("https://piano.metered.live/api/v1/turn/credentials?apiKey="+process.env.NEXT_PUBLIC_TURN_SERVER_API_KEY);
+  //   const stunAndTurnServers = await response.json();
+  //   const servers: object = {
+  //     iceServers: stunAndTurnServers,
+  //       iceCandidatePoolSize: 10,
+  //     };  
+  //   pc = new RTCPeerConnection(servers);
+  //   remoteStream = new MediaStream();
     
-    // localStream.getTracks().forEach((track: any) => {
-    //   pc.addTrack(track, localStream);
-    // });
+  //   // localStream.getTracks().forEach((track: any) => {
+  //   //   pc.addTrack(track, localStream);
+  //   // });
 
-    pc.ontrack = (e: any) => {
-      e.streams[0].getTracks().forEach((track: any) => {
-        console.log("TRYING TO DISPLAY REMOTE STREAM");
-        console.log(track);
-        const hello = remoteStream.addTrack(track);
-        console.log(hello)
-      });
-    }
-    // const myWebcam: HTMLVideoElement = document.getElementById("my-webcam") as HTMLVideoElement;
+  //   pc.ontrack = (e: any) => {
+  //     e.streams[0].getTracks().forEach((track: any) => {
+  //       console.log("TRYING TO DISPLAY REMOTE STREAM");
+  //       console.log(track);
+  //       const hello = remoteStream.addTrack(track);
+  //       console.log(hello)
+  //     });
+  //   }
+  //   // const myWebcam: HTMLVideoElement = document.getElementById("my-webcam") as HTMLVideoElement;
 
-    // console.log(localStream);
-    // console.log(remoteStream);
-    // myWebcam.srcObject = localStream;
-    // myWebcam.play().catch(error => {
-    //   console.error(error)
-    // });
+  //   // console.log(localStream);
+  //   // console.log(remoteStream);
+  //   // myWebcam.srcObject = localStream;
+  //   // myWebcam.play().catch(error => {
+  //   //   console.error(error)
+  //   // });
 
-    //startCall
-    const callDoc = collection(db, 'calls');
-    const callId = (await addDoc(callDoc, {})).id;
-    await updateDoc(doc(callDoc, "newCalls"), {[callId]: {createdAt:serverTimestamp()}, callId: callId})
-    // const callInputField: HTMLInputElement = document.getElementById("callInputField") as HTMLInputElement;
-    // callInputField.value = callId;
+  //   //startCall
+  //   const callDoc = collection(db, 'calls');
+  //   const callId = (await addDoc(callDoc, {})).id;
+  //   await updateDoc(doc(callDoc, "newCalls"), {[callId]: {createdAt:serverTimestamp()}, callId: callId})
+  //   // const callInputField: HTMLInputElement = document.getElementById("callInputField") as HTMLInputElement;
+  //   // callInputField.value = callId;
 
-    const offerCandidates = collection(doc(callDoc, callId), 'offerCandidates');
-    const answerCandidates = collection(doc(callDoc, callId), 'answerCandidates');
+  //   const offerCandidates = collection(doc(callDoc, callId), 'offerCandidates');
+  //   const answerCandidates = collection(doc(callDoc, callId), 'answerCandidates');
 
-    pc.onicecandidate = async (event: any) => {
-      if (event.candidate) {
-        console.log({event_candidate1: event.candidate});
-        await setDoc(doc(offerCandidates), {...event.candidate.toJSON()})
-      }
-    }
+  //   pc.onicecandidate = async (event: any) => {
+  //     if (event.candidate) {
+  //       console.log({event_candidate1: event.candidate});
+  //       await setDoc(doc(offerCandidates), {...event.candidate.toJSON()})
+  //     }
+  //   }
 
-    const offerDescription = await pc.createOffer();
-    await pc.setLocalDescription(offerDescription);
+  //   const offerDescription = await pc.createOffer();
+  //   await pc.setLocalDescription(offerDescription);
 
-    await updateDoc(doc(callDoc, callId), { createdAt: serverTimestamp(), offer: {sdp: offerDescription.sdp, type: offerDescription.type }})
+  //   await updateDoc(doc(callDoc, callId), { createdAt: serverTimestamp(), offer: {sdp: offerDescription.sdp, type: offerDescription.type }})
     
-    onSnapshot(doc(callDoc, callId), (snapshot) => {
-      const data = snapshot.data();
-      console.log(data);
-      if (!pc.currentRemoteDescription && data?.answer) {
-        console.log({data_answer1: data.answer});
-        const answerDescription = new RTCSessionDescription(data.answer);
-        pc.setRemoteDescription(answerDescription);
-      }
-    })
+  //   onSnapshot(doc(callDoc, callId), (snapshot) => {
+  //     const data = snapshot.data();
+  //     console.log(data);
+  //     if (!pc.currentRemoteDescription && data?.answer) {
+  //       console.log({data_answer1: data.answer});
+  //       const answerDescription = new RTCSessionDescription(data.answer);
+  //       pc.setRemoteDescription(answerDescription);
+  //     }
+  //   })
 
-    onSnapshot(answerCandidates, (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        if (change.type === 'added') {
-          console.log({change_doc_data1: change.doc.data()});
-          const candidate = new RTCIceCandidate(change.doc.data());
-          pc.addIceCandidate(candidate);
-        }
-      })
-    })
-    //showVideo
-    console.log("PROVING TRACKS ARE AVAILABLE");
-    console.log(remoteStream.getTracks());
-    console.log(remoteStream);
-    const theirWebcam: HTMLVideoElement = document.getElementById("their-webcam") as HTMLVideoElement;
-    theirWebcam.srcObject = remoteStream;
+  //   onSnapshot(answerCandidates, (snapshot) => {
+  //     snapshot.docChanges().forEach((change) => {
+  //       if (change.type === 'added') {
+  //         console.log({change_doc_data1: change.doc.data()});
+  //         const candidate = new RTCIceCandidate(change.doc.data());
+  //         pc.addIceCandidate(candidate);
+  //       }
+  //     })
+  //   })
+  //   //showVideo
+  //   console.log("PROVING TRACKS ARE AVAILABLE");
+  //   console.log(remoteStream.getTracks());
+  //   console.log(remoteStream);
+  //   const theirWebcam: HTMLVideoElement = document.getElementById("their-webcam") as HTMLVideoElement;
+  //   theirWebcam.srcObject = remoteStream;
     
-    theirWebcam.play().catch(error => {
-      console.error(error)
-    });
+  //   theirWebcam.play().catch(error => {
+  //     console.error(error)
+  //   });
 
-  }
+  // }
 
   return (
     <>
@@ -209,6 +209,7 @@ function SendToHost({ localStream, callId }: { localStream: MediaStream; callId:
       };
     pc = new RTCPeerConnection(servers);
     
+    console.log("send to host local stream", localStream)
     localStream.getTracks().forEach((track: any) => {
       pc.addTrack(track, localStream);
     });
