2aa1c68dfc71566a0a47362b8ed53ff40b2a3c04
calvin <calvinducharme@gmail.com>
2023-09-16 11:19:58 -0700

    um

--- components/audiocall7.tsx
+++ components/audiocall7.tsx
@@ -296,38 +296,8 @@ export function Broadcast() {
 
     return (
         <>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline">Broadcast Settings</Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
-              <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right">
-                  Name
-                </Label>
-                <Input id="name" value="Pedro Duarte" className="col-span-3" />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Username
-                </Label>
-                <Input id="username" value="@peduarte" className="col-span-3" />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button type="submit">Save changes</Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
         <Button onClick={() => {initMedia()}}>initProcess</Button>
-        <p className='text-sm'>{info}</p>
+        {/* <p className='text-sm'>{info}</p> */}
         {callIds.map(callId => (
         <StreamToAudience key={callId} localStream={localStream} callOptions={callOptions} callId={callId} />
       ))}
