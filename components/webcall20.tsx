b5fb1ec39ecf914e01463f6360be3b231cf6f875
Calvin <calvinducharme@gmail.com>
2023-10-16 17:59:45 -0700

    test with config log

--- components/webcall20.tsx
+++ components/webcall20.tsx
@@ -431,10 +431,12 @@ export function BroadcastCall({ callsCollection, localStream, callId, data }: { 
         console.error("dc.onerror", JSON.stringify(event))
       }
             
-      // localStream.getTracks().forEach((track: any) => {
-      //   if (pc) {pc.addTrack(track, localStream)}
-      //   else {console.error("peerConnection was null")}
-      // })
+      localStream.getTracks().forEach((track: any) => {
+        if (pc) {pc.addTrack(track, localStream)}
+        else {console.error("peerConnection was null")}
+      })
+
+      console.log("pc config",pc.getConfiguration())
       
       pc.onconnectionstatechange = (event) => {
         if (pc) {setStatus(pc.connectionState)}
