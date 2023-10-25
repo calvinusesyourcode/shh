5d0cf5bb62701946daf6e4470ed588721a468519
Calvin <calvinducharme@gmail.com>
2023-10-25 00:09:41 -0700

    api test w chatgpt

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,36 @@
 import { NextResponse } from "next/server"
+import OpenAI from "openai";
 
 export async function GET() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1")
-    const data = await res.json()
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    
+    const prompt = `
+    You are a wise sage who has lived for thousands of years and has a deep understanding of the human condition, and your task is to remind me of my fundamental purpose in life, the perplexity of existence, and the beauty of it all with a beautiful, poetic, philosophical speech about loving life.
+    Please use this list of ideas to inspire your speech:
+    - I have a responsibility to my woman to succeed and provide for her
+    - The greek arc of life says I am at the age of going to war becoming a warrior
+    - Nobody else is coming to save me
+    - My favourite people, such as Rogan and Lex, are out there somewhere getting after it today too
+    - My family needs me to be strong because weakness is easy
+    - Good things are always hard
+    - Love and beauty and truth are fundamental by the 2nd law of thermodynamics
+    Please remember my preferences:
+    - Your speech should have the poetic cadence and beauty of a Shakespearian monologue in poetic modern english. Start with simple but bold sentences like "Love is real. Truth is real." and then slowly elaborate into bigger and bigger ideas through story or metaphor. 
+    - When addressing me, you can say "my son", or "little human". or just call me "Calvin'
+    - Please output only your speech, not my prompt or any other text.
+    Thank you in advance for your wisdom, I am grateful for your help.
+    `
+
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: prompt }],
+        model: "gpt-3.5-turbo",
+        max_tokens: 200,
+    });
+
+    const data = completion.choices[0].message.content
    
     return NextResponse.json({ data })
 }
+
--- app/api2/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET() {
-    const data = { message: "Hello World" }
-   
-    return NextResponse.json({ data })
-}
