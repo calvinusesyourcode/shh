01b4cedf46a745fbb5ae11b6ebb61034eda9698d
Calvin <calvinducharme@gmail.com>
2023-10-25 00:25:45 -0700

    up

--- app/api0/route.ts
+++ app/api0/route.ts
@@ -23,14 +23,8 @@ export async function GET() {
     Thank you in advance for your wisdom, I am grateful for your help.
     `
 
-    // const completion = await openai.chat.completions.create({
-    //     messages: [{ role: "system", content: prompt }],
-    //     model: "gpt-3.5-turbo",
-    //     max_tokens: 200,
-    // });
-
-    // const data = completion.choices[0].message.content
+    const data = prompt
    
-    return NextResponse.json({ prompt })
+    return NextResponse.json({ data })
 }
 
